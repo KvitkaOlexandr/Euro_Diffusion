e0b1bd0cd3d6789a205ec10d45a90cbcb35e36b7
oleksandr.kvitka <oleksandr.kvitka@unity-bars.com>
2020-10-27 17:55:09 +0200

    save_workday

--- Euro_diffusion/City.cs
+++ Euro_diffusion/City.cs
@@ -7,12 +7,14 @@ namespace Euro_diffusion
     public class City
     {
         public const int INITIAL_BALANCE = 1000000;
+        public const int PAY_PERCENT = 1000;
 
         public String Country;
         public int X;
         public int Y;
         public int Balance;
         public int CachedIncome;
+        public List<City> Neighbors;
 
         public City(String country, int x, int y, int initialBalance = INITIAL_BALANCE)
         {
